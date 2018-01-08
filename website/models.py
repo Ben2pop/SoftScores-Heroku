de29bc0cf83a2053df4fc85cac4d5952303db02a
Raphael Bendenoun <raphaelbendenoun@MacBook-Pro-de-Raphael.local>
2018-01-08 09:23:13 +0200

    debug add global

--- website/models.py
+++ website/models.py
@@ -28,7 +28,7 @@ class Project(models.Model):
         try:
             x = Project.objects.get(id = self.id).team_id.members.all()
             for i in x:
-                result = 1
+                global result = 1
                 if i.response_set.exists():
                     result = result * True
                 else:
