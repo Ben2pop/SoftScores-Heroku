890a37fdeca2fca6ace5807cc4b62f1a84525b98
Raphael Bendenoun <raphaelbendenoun@MacBook-Pro-de-Raphael.local>
2018-01-08 14:44:46 +0200

    change email conf

--- Authentication_project/prod.py
+++ Authentication_project/prod.py
@@ -20,7 +20,7 @@ DATABASES = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=465
+EMAIL_PORT=587
 EMAIL_HOST_USER = 'softscoresapp@gmail.com'
 EMAIL_HOST_PASSWORD = 'SoftScoresTelAviv'
 DEFAULT_EMAIL_FROM = 'softscoresapp@gmail.com'
