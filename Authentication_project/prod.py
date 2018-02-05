761f15304140aac23ac482f6b34d593e3779a1e8
Raphael Bendenoun <raphaelbendenoun@MacBook-Pro-de-Raphael.local>
2018-02-05 15:18:27 +0200

    mail

--- Authentication_project/prod.py
+++ Authentication_project/prod.py
@@ -22,6 +22,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER = 'softscoresapp@gmail.com'
-EMAIL_HOST_PASSWORD = 'SoftScoresTelAviv'
+EMAIL_HOST_PASSWORD = 'SoftScoresAppTelAviv'
 DEFAULT_EMAIL_FROM = 'softscoresapp@gmail.com'
 EMAIL_USE_TLS = True
